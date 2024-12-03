2113d94c6256f2da0a0016d14d004a982b0d65cd
강민서 <138178615+kms199406@users.noreply.github.com>
2024-12-04 01:17:01 +0900

    202411161435 <작업자> 강민서 <작업내용> 패키지별 정리

--- scripts/set_kibana_password.sh
+++ scripts/set_kibana_password.sh
@@ -14,12 +14,9 @@ response=$(curl -X POST "${ELASTICSEARCH_URL}/_security/service/kibana/credentia
 token=$(echo $response | jq -r '.token.value')
 
 if [ ! -z "$token" ]; then
-  # 환경 변수로 토큰 설정
-  export ELASTICSEARCH_SERVICE_TOKEN=$token
-  echo "Service token이 성공적으로 생성되었습니다."
-
-  # kibana.yml 설정에도 반영
-  sed -i "s/elasticsearch.username:.*$/elasticsearch.serviceaccounttoken: $token/" /usr/share/kibana/config/kibana.yml
+  # .env 파일에 저장
+  echo "ELASTICSEARCH_SERVICE_TOKEN=$token" >> .env
+  echo "Service token이 성공적으로 생성되어 .env에 저장되었습니다."
   exit 0
 else
   echo "Service token 생성 실패"
