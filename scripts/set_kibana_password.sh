fd950a10b9f6b2f4be37b666ea0305d38e61c87b
kms199406 <sksdmltkek14@gmail.com>
2024-12-03 21:40:46 +0900

    sd

--- scripts/set_kibana_password.sh
+++ scripts/set_kibana_password.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
-# Elasticsearch 내부 도커 네트워크 주소 사용
-ELASTICSEARCH_URL="https://elasticsearch:9200"
+# 설정
+ELASTICSEARCH_URL="https://elasticsearch.www.projectkkk.com:9200"
 
-# 서비스 계정 생성 및 토큰 발급
-curl -X POST "${ELASTICSEARCH_URL}/_security/service/elastic/kibana/credential/token/create" \
+# 서비스 계정 토큰 생성
+response=$(curl -X POST "${ELASTICSEARCH_URL}/_security/service/kibana/credential/token/create" \
   -H "Content-Type: application/json" \
   -u "elastic:${ELASTIC_PASSWORD}" \
-  --cacert /usr/share/kibana/config/kibana.pem
+  --cacert /usr/share/kibana/config/kibana.pem \
+  --silent)
 
-# 또는 kibana_system 사용자 비밀번호 설정
-curl -X POST "${ELASTICSEARCH_URL}/_security/user/kibana_system/_password" \
-  -H "Content-Type: application/json" \
-  -u "elastic:${ELASTIC_PASSWORD}" \
-  -d "{\"password\":\"${KIBANA_PASSWORD}\"}" \
-  --cacert /usr/share/kibana/config/kibana.pem
+# 응답에서 토큰 추출
+token=$(echo $response | jq -r '.token.value')
+
+if [ ! -z "$token" ]; then
+  # 환경 변수로 토큰 설정
+  export ELASTICSEARCH_SERVICE_TOKEN=$token
+  echo "Service token이 성공적으로 생성되었습니다."
+
+  # kibana.yml 설정에도 반영
+  sed -i "s/elasticsearch.username:.*$/elasticsearch.serviceaccounttoken: $token/" /usr/share/kibana/config/kibana.yml
+  exit 0
+else
+  echo "Service token 생성 실패"
+  exit 1
+fi
